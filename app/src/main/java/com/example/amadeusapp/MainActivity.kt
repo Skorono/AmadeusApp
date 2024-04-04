78b06ee32142ddad8fed2d2ddc05733772d10c0d
listik <mikhail.popov221@gmail.com>
2024-04-04 04:49:29 +0300

    Design ui

--- app/src/main/java/com/example/amadeusapp/MainActivity.kt
+++ app/src/main/java/com/example/amadeusapp/MainActivity.kt
@@ -5,18 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -39,7 +38,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoginPage() {
-    Column {
+    var userId by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.background(color = Color.Black)) {
         Row(){
             Image(bitmap = ImageBitmap.imageResource(
                 id = R.drawable.amadeus_logo2),
@@ -48,24 +50,29 @@ fun LoginPage() {
             )
         }
 
-        Column {
+        Column(Modifier.padding(top = Dp(50f), start = Dp(10f))) {
             Row(){
-                Text( text = "USER_ID:" )
+                Text(
+                    text = "USER_ID:",
+                    color = Color.LightGray
+                )
+
                 Spacer(modifier = Modifier.width(Dp(5f)))
                 TextField(
                     value = "",
-                    onValueChange = {}
+                    onValueChange = {it -> userId += it},
                 )
             }
 
             Row(){
                 Text(
                     text = "PASSWORD:",
+                    color = Color.LightGray,
                 )
                 Spacer(modifier = Modifier.width(Dp(5f)))
                 TextField(
                     value = "",
-                    onValueChange = {}
+                    onValueChange = {it -> password += it},
                 )
             }
         }
@@ -74,7 +81,7 @@ fun LoginPage() {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     AmadeusAppTheme {
         LoginPage()
     }
